216aa8e73cb8ddddb98c5a21ce9b097e50611228
sk <sk@httplab.ru>
2012-01-08 05:11:14 +0400

    Сохранение фиксов

--- app.rb
+++ app.rb
@@ -8,16 +8,41 @@ configure do
 
   require 'erb'
   @db_settings = YAML::load(ERB.new(IO.read("config/database.yml")).result)
-  FileUtils.mkdir_p("log")
+  ActiveRecord::Base.establish_connection @db_settings['development']
 end
 
-post '/:fix' do
-  params.each_pair do |k, v|
-    puts "#{k} = [#{v}]"
+get '/fix' do
+  redirect '/foxes'
+end
+
+post '/fix' do
+  p "Fix: [#{params.inspect}]"
+  Device.find_or_create_by_device_id_and_sim_id :device_id => params[:device_id], :sim_id => params[:sim_id]
+  fix = Fix.create :lat => params[:lat],
+                   :lon => params[:lon],
+                   :alt => params[:alt],
+                   :acc => params[:acc],
+                   :client_time => params[:client_time] || Time.now,
+                   :provider_id => params[:provider_id],
+                   :device_id => params[:device_id],
+                   :user_id => params[:user_id],
+                   :speed => params[:speed],
+                   :bearing => params[:bearing]
+  content_type :json
+  if fix.save
+    Fox.all.to_json
+  else
+    puts "Error: #{fix.errors.first.full_messages}"
+    error 400, fix.errors.to_json
   end
 end
 
-get '/:foxes' do
+get '/foxes' do
+  content_type :json
+  Fox.all.to_json
+end
+
+post '/foxes' do
   content_type :json
   Fox.all.to_json
 end
--- db/migrate/20120107212345_create_foxes.rb
+++ db/migrate/20120107212345_create_foxes.rb
@@ -3,7 +3,7 @@ class CreateFoxes < ActiveRecord::Migration
     create_table :foxes do |t|
       t.string :name, :limit => 45, :null => false
       t.decimal :lat, :precision => 16, :scale => 13, :null => false, :default => '47.228752'
-      t.decimal :lon, :precision => 16, :scale => 13, :null => false, :default => '47.228752'
+      t.decimal :lon, :precision => 16, :scale => 13, :null => false, :default => '39.715587'
     end
   end
 end
